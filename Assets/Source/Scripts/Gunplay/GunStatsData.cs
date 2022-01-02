ab871e71be31340ad2a7865ff867cb369d73e4e5
Vo1z <vitya.voody@gmail.com>
2022-01-03 00:07:13 +0200

    voiz-guns 1
    -Different sway configurations fow gus were added
    -AR15 data was modified
    -HudRotator was extended with new rotation logic

--- Assets/Source/Scripts/Gunplay/GunStatsData.cs
+++ Assets/Source/Scripts/Gunplay/GunStatsData.cs
@@ -8,12 +8,43 @@ namespace Ingame.Guns
     {
         [BoxGroup("Hud stats")]
         [SerializeField] [Range(0, 10)] private float rotationSpeed = 5;
+        
+        [BoxGroup("Hud stats"), Space]
+        [SerializeField] [Range(0, 50)] private float maxRotationAngleX = 20;
+        [BoxGroup("Hud stats")]                                           
+        [SerializeField] [Range(0, 20)] private float rotationAngleMultiplierX = 20;
+        [BoxGroup("Hud stats")] 
+        [SerializeField] private bool inverseRotationX = false;
+        
+        [BoxGroup("Hud stats"), Space]
+        [SerializeField] [Range(0, 50)] private float maxRotationAngleY = 20;
+        [BoxGroup("Hud stats")]                                                      
+        [SerializeField] [Range(0, 40)] private float rotationAngleMultiplierY = 20;
+        [BoxGroup("Hud stats")] 
+        [SerializeField] private bool inverseRotationY = false;
+
+        [BoxGroup("Hud stats"), Space]
+        [SerializeField] [Range(0, 50)] private float maxRotationAngleZ = 20;
+        [BoxGroup("Hud stats")]                                                      
+        [SerializeField] [Range(0, 40)] private float rotationAngleMultiplierZ = 20; 
+        [BoxGroup("Hud stats")] 
+        [SerializeField] private bool inverseRotationZ = false;
+        
         [BoxGroup("Gun stats")]
         [SerializeField] private FireMode[] fireModes;
         
-        public const float MAX_ROTATION_ANGLE = 20f;
-
         public float RotationSpeed => rotationSpeed;
+        public float MaxRotationAngleX => maxRotationAngleX;
+        public float RotationAngleMultiplierX => rotationAngleMultiplierX;
+        public float InverseRotationX => inverseRotationX ? -1: 1;
+        
+        public float MaxRotationAngleY => maxRotationAngleY;
+        public float RotationAngleMultiplierY => rotationAngleMultiplierY;
+        public float InverseRotationY => inverseRotationY ? -1: 1;
+        
+        public float MaxRotationAngleZ => maxRotationAngleZ;
+        public float RotationAngleMultiplierZ => rotationAngleMultiplierZ;
+        public float InverseRotationZ => inverseRotationZ ? -1: 1;
         
         public FireMode[] FireModes => fireModes;
     }
--- Assets/Source/Scripts/Player/HUD/HudRotator.cs
+++ Assets/Source/Scripts/Player/HUD/HudRotator.cs
@@ -15,6 +15,7 @@ namespace Ingame.Player.HUD
         private Quaternion _initialGunLocalRotation;
         private Transform _gunTransform;
         private Gun _gun;
+        private GunStatsData _gunStats;
 
         private void Awake()
         {
@@ -28,20 +29,36 @@ namespace Ingame.Player.HUD
 
         private void RotateGun(Vector2 deltaRotationInput)
         {
-            if(_gun == null || _gunTransform == null)
+            if (_gun == null || _gunTransform == null || _gunStats == null)
                 return;
 
             var deltaInputInAngle = deltaRotationInput * ANGLE_FOR_ONE_SCREEN_PIXEL;
-            var rotationAngle = GunStatsData.MAX_ROTATION_ANGLE * Mathf.Clamp(deltaInputInAngle.x, -1, 1);
-            var rotationOffset = _gun.GunStatsData.RotationSpeed * Time.deltaTime;
-            var targetRotation = _initialGunLocalRotation * Quaternion.AngleAxis(rotationAngle, Vector3.forward);
             
+            var xRotationAngle = _gunStats.RotationAngleMultiplierX * Mathf.Clamp(deltaInputInAngle.y, -1, 1);
+            xRotationAngle = Mathf.Clamp(xRotationAngle, -_gunStats.MaxRotationAngleX, _gunStats.MaxRotationAngleX);
+            xRotationAngle *= _gunStats.InverseRotationX;
+
+            var yRotationAngle = _gunStats.RotationAngleMultiplierY * Mathf.Clamp(deltaInputInAngle.x, -1, 1);
+            yRotationAngle = Mathf.Clamp(yRotationAngle, -_gunStats.MaxRotationAngleY, _gunStats.MaxRotationAngleY);
+            yRotationAngle *= _gunStats.InverseRotationY;
+            
+            var zRotationAngle = _gunStats.RotationAngleMultiplierZ * Mathf.Clamp(deltaInputInAngle.x, -1, 1);
+            zRotationAngle = Mathf.Clamp(zRotationAngle, -_gunStats.MaxRotationAngleZ, _gunStats.MaxRotationAngleZ);
+            zRotationAngle *= _gunStats.InverseRotationZ;
+            
+            var rotationOffset = _gunStats.RotationSpeed * Time.deltaTime;
+            var targetRotation = _initialGunLocalRotation *
+                                 Quaternion.AngleAxis(xRotationAngle, Vector3.right) *
+                                 Quaternion.AngleAxis(yRotationAngle, Vector3.up) *
+                                 Quaternion.AngleAxis(zRotationAngle, Vector3.forward);
+
             _gunTransform.localRotation = Quaternion.Slerp(_gunTransform.localRotation, targetRotation, rotationOffset);
         }
 
         private void PlaceGunInHands(Gun gun)
         {
             _gun = gun;
+            _gunStats = _gun.GunStatsData;
             _gunTransform = _gun.transform;
             _gunTransform.parent = _playerHUD.transform;
             _initialGunLocalRotation = _gunTransform.localRotation;
@@ -53,6 +70,7 @@ namespace Ingame.Player.HUD
         {
             _gunTransform.parent = null;
             _gun = null;
+            _gunStats = null;
             _gunTransform = null;
             
             _playerInputReceiver.OnRotationDeltaInputReceived -= RotateGun;
